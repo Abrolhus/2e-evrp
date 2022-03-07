eecb758f8c18d3779e5fecfaf8decb76dc0b2e03
Samuel Abreu <samuelapp@outlook.com>
2022-03-07 17:56:57 -0300

    temporary commit

---
+++ src/Ev.h
@@ -0,0 +1,31 @@
+#ifndef EV_H
+#include "Instance.h"
+#include <vector>
+#define EV_H
+class EvNode{
+public:
+    EvNode(int nodeId, double time0, double time1, double battery) : 
+            nodeId(nodeId), time0(time0), time1(time1), battery(battery){ }
+    EvNode(int nodeId, double time0, double battery) : 
+            nodeId(nodeId), time0(time0), battery(battery){ 
+        time1 = time0
+    }
+    int nodeId;
+    double time0; // time before service
+    double time1; // time after service (needed if recharging)
+    double battery; // remaining battery
+};
+class Ev{
+private:
+    std::vector<EvNode> route;
+    float totalDistance;
+    int satelite;
+    float totalDemand;
+    float maxCapacity;
+public:
+    float getRemainingBattery();
+    float getRemainingCapacity();
+    const EvNode& at(int i);
+    float getDemandOf(int i, const Instance& inst);
+};
+#endif
